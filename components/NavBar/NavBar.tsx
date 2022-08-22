9758e777f8bf19b01461602a4817fb27095af5b5
Liam <liammorley123@live.co.uk>
2022-08-22 19:52:45 +0100

    updates

---
+++ components/NavBar/NavBar.tsx
@@ -0,0 +1,23 @@
+import type { NextComponentType, NextPage } from 'next'
+import styles from './NavBar.module.css'
+import Link from 'next/link'
+
+const NavBar: NextComponentType = () => {
+  return (
+    <div className={styles.navFlex}>
+    <div>
+      <h2>GO-SK</h2>
+    </div>
+    <div className={styles.linkFlex}>
+      <Link href="/">
+        <a>Jobs</a>
+      </Link>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+    </div>
+  </div>
+  )
+}
+
+export default NavBar
