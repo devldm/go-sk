d59b38ebf47f59ec04c10a420f85f42c35cda396
Liam <liammorley123@live.co.uk>
2022-09-03 09:49:25 +0100

    style updates and remove about page

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -5,18 +5,17 @@ import Link from 'next/link'
 const NavBar: NextComponentType = () => {
   return (
     <div className={styles.navFlex}>
-    <div>
-      <h2>GO-SK</h2>
+      <div>
+        <Link href="/">
+          <h2>GO-SK</h2>
+        </Link>
+      </div>
+      <div className={styles.linkFlex}>
+        <Link href="/jobs">
+          <a>Jobs</a>
+        </Link>
+      </div>
     </div>
-    <div className={styles.linkFlex}>
-      <Link href="/jobs">
-        <a>Jobs</a>
-      </Link>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </div>
-  </div>
   )
 }
 
