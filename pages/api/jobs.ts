46583da8ac4217cbac8cd8833488c078a52e74ea
Liam <liammorley123@live.co.uk>
2022-10-09 17:58:05 +0100

    uncomment postData fields

--- pages/api/jobs.ts
+++ pages/api/jobs.ts
@@ -10,7 +10,6 @@ export default async function handler(
   if (req.method !== "POST") {
     return res.status(405).json({ message: "Method not allowed" });
   }
-
   try {
     const postData: Job = await prisma.go_sk_jobs.create({ // TODO: turn this back to jobs from  any
       data: {
@@ -19,8 +18,8 @@ export default async function handler(
         job_title: req.body.job_title,
         company_name: req.body.company_name,
         location: req.body.location,
-        //apply_url: req.body.apply_url,
-        //linkedin_url: req.body.linkedin_url,
+        apply_url: req.body.apply_url,
+        linkedin_url: req.body.linkedin_url,
       },
     });
   } catch (err) {
