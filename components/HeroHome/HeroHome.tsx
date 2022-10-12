a57e6476bc3bb9261275c51a90ab49dea1a579e6
Liam <liammorley123@live.co.uk>
2022-10-12 14:43:11 +0100

    home component and button updates

--- components/HeroHome/HeroHome.tsx
+++ components/HeroHome/HeroHome.tsx
@@ -1,19 +1,25 @@
 import styles from "./HeroHome.module.css";
 import Image from "next/future/image";
-import skImage from "../../public/skImage1.jpg";
+import skImage from "../../public/skImage2.jpg";
 import Link from "next/link";
 
 export default function HeroHome() {
   return (
     <div className={styles.heroHomeContainer}>
       <div className={styles.heroText}>
-        <h1 className={styles.title}>Start your journey to South Korea</h1>
+        <h1 className={styles.title}>
+          Start your journey to <br />
+          <span className={styles.cityName}>South Korea</span>
+        </h1>
 
         <p className={styles.description}>
-          Find your dream role in South Korea{" "}
+          We aim to be the bridge between the worlds talent and South Korea's
+          vibrant economy{" "}
         </p>
 
-        <Link href="/jobs"><button className={styles.jobsCta}>See our Jobs</button></Link>
+        <Link href="/jobs">
+          <button className={styles.jobsCta}>See our Jobs</button>
+        </Link>
       </div>
       <div className={styles.heroImage}>
         <Image src={skImage} className={styles.landingImage} priority />
