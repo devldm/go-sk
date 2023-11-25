f9febd08233f01a6b0499b61b2690ca5398b95a9
liam <39243060+devldm@users.noreply.github.com>
2023-11-25 11:47:58 +0900

    update job details to be fixed

--- components/JobDetails/JobDetails.tsx
+++ components/JobDetails/JobDetails.tsx
@@ -1,5 +1,6 @@
+import Link from "next/link";
 import { Job } from "../../types";
-import styles from "./JobDetails.module.css";
+import Button from "../Button/Button";
 
 interface Props {
   job: Job;
@@ -14,41 +15,55 @@ export default function JobDetails({ job }: Props) {
     job.remote_level;
 
   return (
-    <>
+    <div className="lg:fixed min-w-max max-w-[400px]">
       {noJobDetails && (
-        <div className="bg-[#222] p-4 rounded-xl mb-5 flex flex-col">
-          <h3 className="my-3 text-2xl font-bold">Job Details</h3>
-          <hr className="mb-2" />
-          <div className={"flex gap-4 flex-col"}>
-            {job.role_type && (
-              <div className={""}>
-                <p className="font-thin min-w-max text-lg">Role type</p>
-                <p className="min-w-max text-xl">{job.role_type}</p>
-              </div>
-            )}
-            {job.experience_level && (
-              <div className={""}>
-                <p className="font-thin min-w-max text-lg">Experience level</p>
-                <p className="min-w-max text-xl">{job.experience_level}</p>
-              </div>
-            )}
-            {job.remote_level && (
-              <div className={""}>
-                <p className="font-thin min-w-max text-lg">Remote level</p>
-                <p className="min-w-max text-xl">{job.remote_level}</p>
-              </div>
-            )}
-            {job.salary_min! > 0 && job.salary_max! > 0 && (
-              <div className={""}>
-                <p className="font-thin min-w-max text-lg">
-                  Salary range {job.currency && `(${job.currency})`}
-                </p>
-                <p className="min-w-max text-xl">{`${job.salary_min} - ${job.salary_max}`}</p>
-              </div>
-            )}
+        <>
+          <div className="bg-[#222] p-4 rounded-xl mb-5 flex flex-col">
+            <h3 className="my-3 text-2xl font-bold">Job Details</h3>
+            <hr className="mb-2" />
+            <div className={"flex gap-4 flex-col"}>
+              {job.role_type && (
+                <div className={""}>
+                  <p className="font-thin min-w-max text-lg">Role type</p>
+                  <p className="min-w-max text-xl">{job.role_type}</p>
+                </div>
+              )}
+              {job.experience_level && (
+                <div className={""}>
+                  <p className="font-thin min-w-max text-lg">
+                    Experience level
+                  </p>
+                  <p className="min-w-max text-xl">{job.experience_level}</p>
+                </div>
+              )}
+              {job.remote_level && (
+                <div className={""}>
+                  <p className="font-thin min-w-max text-lg">Remote level</p>
+                  <p className="min-w-max text-xl">{job.remote_level}</p>
+                </div>
+              )}
+              {job.salary_min! > 0 && job.salary_max! > 0 && (
+                <div className={""}>
+                  <p className="font-thin min-w-max text-lg">
+                    Salary range {job.currency && `(${job.currency})`}
+                  </p>
+                  <p className="min-w-max text-xl">{`${job.salary_min} - ${job.salary_max}`}</p>
+                </div>
+              )}
+            </div>
           </div>
-        </div>
+          <Button
+            applyUrl={job.apply_url ?? "No url"}
+            buttonText={"Apply"}
+            customClassName="w-[100%] min-w-full"
+          />
+          <Link href="/jobs">
+            <p className="text-[#3758f9] font-bold text-right text-xl cursor-pointer mt-3 mb-5 block">
+              Go back to Jobs
+            </p>
+          </Link>
+        </>
       )}
-    </>
+    </div>
   );
 }
--- pages/jobs/[id].tsx
+++ pages/jobs/[id].tsx
@@ -1,8 +1,6 @@
 import type { InferGetStaticPropsType, NextPage } from "next";
 import prisma from "../../db";
 import Layout from "../../components/Layout/Layout";
-import Link from "next/link";
-import Button from "../../components/Button/Button";
 import { getTimeSincePosting } from "../../utils/getTimeSincePosting";
 import JobDetails from "../../components/JobDetails/JobDetails";
 
@@ -47,7 +45,7 @@ const Job: NextPage<JobPosting> = ({
 }: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <Layout pageTitle={"Job details"}>
-      <div className="flex gap-4 justify-center m-auto flex-col p-5 lg:w-[80%] lg:flex-row pt-10">
+      <div className="flex md:gap-12 justify-center m-auto flex-col p-5 lg:w-[70%] lg:flex-row pt-10">
         <div className="mb-6 flex-col flex max-w-max">
           <h1 className="md:mt-4 text-4xl font-bold ">{job.job_title}</h1>
           <p className="">{job.location}</p>
@@ -60,25 +58,15 @@ const Job: NextPage<JobPosting> = ({
             )}
           </div>
           <div
-            className="text-xl pt-10"
+            className="text-xl pt-10 break-all"
             dangerouslySetInnerHTML={{
               __html: job.job_description ?? "no content",
             }}
           />
         </div>
 
-        <div className="md:w-[25em]">
+        <div className="md:w-[30em]">
           <JobDetails job={job} />
-          <Button
-            applyUrl={job.apply_url ?? "No url"}
-            buttonText={"Apply"}
-            customClassName="w-[100%] min-w-full"
-          />
-          <Link href="/jobs">
-            <p className="text-[#3758f9] font-bold text-right text-xl cursor-pointer mt-3 mb-5 block">
-              Go back to Jobs
-            </p>
-          </Link>
         </div>
       </div>
     </Layout>
